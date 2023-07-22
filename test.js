7f7096b8baea8674c620f742109c65da08e67764
Maruf Ahmed <marufsarkar512@gmail.com>
2023-07-22 09:35:06 +0600

    added loops and all

--- test.js
+++ test.js
@@ -1,3 +1,8 @@
+// function
+function func1() {
+    console.log("This is a Function.");
+}
+
 // arrow function
 const name = () => {
     return "hello";
@@ -6,6 +11,7 @@ const name = () => {
 // veriables
 const name1 = "ghost";
 const age = 20;
+let number = 1;
 
 // conditional veriable
 const statement = age > 15 ? name1 : "Shadow";
@@ -27,3 +33,12 @@ console.log(anArray);
 anArray.forEach((parts) => {
     console.log(parts);
 });
+
+// while loop
+while (true) {
+    console.log(number);
+    number = number + 1;
+    if (number > 10) {
+        break;
+    }
+}
